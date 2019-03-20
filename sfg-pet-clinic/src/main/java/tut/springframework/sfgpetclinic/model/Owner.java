4bfe3f261691ac0e50614946f86238e70e6b53b1
DayaSatheesh1 <DayaSatheesh1@DayaSatheesh>
2019-03-20 23:28:26 +0530

    Added to repo

---
+++ sfg-pet-clinic/src/main/java/tut/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package tut.springframework.sfgpetclinic.model;
+
+public class Owner extends Person{
+
+}
---
+++ sfg-pet-clinic/src/main/java/tut/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package tut.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
